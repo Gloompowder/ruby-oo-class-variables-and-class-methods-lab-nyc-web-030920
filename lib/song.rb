0543cd8d035cc9ce54d54af04d923b6a535928d3
William Lin <willlin1996@gmail.com>
2020-03-12 02:56:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,47 +1,27 @@
-class Song 
-  attr_accessor :name, :artist, :genre 
-  
-  @@count = 0
-  @@artists = [] 
-  @@genres = []
-  
-  def initialize(title, artist, genre)
+class Song
+  attr_accessor :name, :artist, :genre
 
-      @name = title
-      @artist = artist 
-      @genre = genre
-    if title 
-      @@count += 1
-      @@genres << genre 
-      @@artists << artist
-    end
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
   end
-  
+
   def self.count
     @@count
   end
-  
+
   def self.artists
-    unique_artists = @@artists.uniq
+    @@artists.uniq
   end
-  
-  def self.genres 
-    unique_genres = @@genres.uniq
-  end
-  
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
-    self.genres
-  end 
+
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
@@ -53,3 +33,20 @@ class Song
     end
     artist_count
   end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+end
